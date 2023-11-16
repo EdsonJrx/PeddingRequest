02ec9d8fdb386c4f79a64bfcefa84e2efdec18fe
EdsonJrx <edinhoa7x@gmail.com>
2023-11-16 09:49:25 -0400

    change and remove file from folder apis/list

--- src/apis/list/config.ts
+++ src/apis/list/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_BASE_URL_P,
+    baseURL: process.env.EXPO_PUBLIC_BASE_URL,
     timeout: 10000,
-    headers: {'Authorization': process.env.EXPO_PUBLIC_API_KEY_P}
+    headers: {'Authorization': process.env.EXPO_PUBLIC_API_KEY}
 });
